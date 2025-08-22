912d768d7214feceb4d835c33a7bfe15536b3370
Stephan Gazarov <sgzrov@gmail.com>
2025-08-21 20:04:20 -0700

    Remove Contact Support page and EmailCollector component - Delete ContactSupport.tsx page - Delete EmailCollector.tsx component - Remove Contact Support route from App.tsx - Remove Contact Support link from footer - Remove EmailCollector usage from Index.tsx - Clean up website to remove contact and email collection functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
-import ContactSupport from "./pages/ContactSupport";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -21,7 +20,6 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/terms-of-service" element={<TermsOfService />} />
-          <Route path="/contact-support" element={<ContactSupport />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
