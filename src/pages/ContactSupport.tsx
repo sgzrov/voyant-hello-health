90217a2abc3aad10c431b5b2092ddb4500727653
Stephan Gazarov <sgzrov@gmail.com>
2025-08-21 19:16:32 -0700

    Fix Contact Support: use current Gmail compose URL format to resolve 404 errors

--- src/pages/ContactSupport.tsx
+++ src/pages/ContactSupport.tsx
@@ -7,8 +7,8 @@ const ContactSupport = () => {
     const subject = encodeURIComponent("Voyant Support Request");
     const body = encodeURIComponent("Hi Stephan,\n\nI need help with Voyant:\n\n");
 
-    // Use the correct Gmail compose URL
-    const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=sgzrov@gmail.com&su=${subject}&body=${body}`;
+    // Use Gmail's current compose URL format
+    const gmailUrl = `https://mail.google.com/mail/u/0/#compose?to=sgzrov@gmail.com&subject=${subject}&body=${body}`;
 
     // Try to open Gmail compose window
     window.open(gmailUrl, '_blank');
